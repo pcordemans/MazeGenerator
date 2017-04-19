cd19636e6c825745fca2fa428928a57070bf7305
Piet Cordemans <piet.cordemans@kuleuven.be>
2017-04-19 17:41:40 +0200

    maze generated, needs entry and exit

--- src/amazed/Maze.java
+++ src/amazed/Maze.java
@@ -1,40 +1,102 @@
 package amazed;
 
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Maze {	
 	private char grid [][];
 	private int size;
+	private int gridSize;
 	private static final char wall = 'X';
 	private static final char unvisited = 'U';
-	private int outer;
+	private static final char corridor = ' ';
+	private int outerWallPosition;
 	private RandomList<Coordinate> listOfWalls = new RandomList<Coordinate>();
 	
 	
 	public Maze(int size){
 		this.size = size;
-		outer = size - 1;
-		grid = new char[size][size];
+		gridSize = size * 2 + 1;
+		outerWallPosition = gridSize - 1;
+		grid = new char[gridSize][gridSize];
 		initialise();
+		pickStartCell();
+		constructMaze();
+		makeEntryAndExit();
+	}
+	
+	private void makeEntryAndExit(){
+		int randomRow = randomPosition();
+	}
+	
+	private void constructMaze(){
+		while(!listOfWalls.isEmpty()){
+			Coordinate position = listOfWalls.removeRandom();
+			boolean newCellFound = visitCell(position.row(), position.col());
+			if(newCellFound)grid[position.row()][position.col()] = corridor;			
+		}
+	}
+	
+	private boolean visitCell(int row, int col){
+		if(grid[row + 1][col] == unvisited){
+			grid[row + 1][col] = corridor;
+			addSurroundingWalls(row + 1, col);
+			return true;
+		}
+		if(grid[row - 1][col] == unvisited){
+			grid[row - 1][col] = corridor;
+			addSurroundingWalls(row - 1, col);
+			return true;
+		}
+		if(grid[row][col + 1] == unvisited){
+			grid[row][col + 1] = corridor;
+			addSurroundingWalls(row, col + 1);
+			return true;
+		}
+		if(grid[row][col - 1] == unvisited){
+			grid[row][col - 1] = corridor;
+			addSurroundingWalls(row, col - 1);
+			return true;
+		}
+		return false;
+	}
+	
+	private int randomPosition(){
+		return ThreadLocalRandom.current().nextInt(0, size) * 2 + 1;
+	}
+	
+	private void pickStartCell(){
+		int row = randomPosition();
+		int col = randomPosition();
+		grid[row][col] = corridor;
+		addSurroundingWalls(row, col);		
+	}
+	
+	private void addSurroundingWalls(int row, int col){
+		if(row > 1 && grid[row - 2][col] == unvisited && grid[row-1][col] == wall) listOfWalls.add(new Coordinate(row-1, col));
+		if(row < gridSize - 2 && grid[row + 2][col] == unvisited && grid[row+1][col] == wall) listOfWalls.add(new Coordinate(row+1, col));
+		if(col > 1 && grid[row][col-2] == unvisited && grid[row][col-1] == wall) listOfWalls.add(new Coordinate(row, col-1));
+		if(col < gridSize - 2 && grid[row][col+2] == unvisited && grid[row][col+1] == wall) listOfWalls.add(new Coordinate(row, col+1));
 	}
 	
 	private void initialise(){
-		for(int row = 0; row < size; row++){
-			for(int col = 0; col < size; col++){
+		for(int row = 0; row < gridSize; row++){
+			for(int col = 0; col < gridSize; col++){
 				if(row == 0) grid[row][col] = wall;
 				else if(col == 0) grid[row][col] = wall;
-				else if(row == outer) grid[row][col] = wall;
-				else if(col == outer) grid[row][col] = wall;
+				else if(row == outerWallPosition) grid[row][col] = wall;
+				else if(col == outerWallPosition) grid[row][col] = wall;
 				else if(col % 2 != 0 && row % 2 != 0) grid[row][col] = unvisited;
 				else grid[row][col] = wall;
 			}
 		}
 	}
 	
-	private void print(){
-		for(int i = 0; i < size; i++){
+	public void print(){
+		for(int i = 0; i < gridSize; i++){
 			System.out.println(grid[i]);
 		}
+		System.out.println("");
 	}
 	
 	private class Coordinate{
@@ -45,11 +107,11 @@ public class Maze {
 			ycor = y;
 		}
 		
-		public int X(){
+		public int row(){
 			return xcor;
 		}
 		
-		public int Y(){
+		public int col(){
 			return ycor;
 		}
 	}
@@ -60,7 +122,7 @@ public class Maze {
 		int size = scanner.nextInt();
 		scanner.close();
 		System.out.println("Generating maze...");
-		Maze maze = new Maze(size * 2 + 1);
+		Maze maze = new Maze(size);
 		maze.print();
 	}
 }
